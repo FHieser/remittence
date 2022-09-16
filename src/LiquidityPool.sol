8b2792f41656323c422fd6787585fe5370079606
FHieser <felix_hieser@web.de>
2022-09-16 21:28:45 +0200

    Add Basic LiquidityPool Structure

---
+++ src/LiquidityPool.sol
@@ -0,0 +1,47 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+contract LiquidityPool {
+    string name;
+    uint256 fiatPool;
+    address usdc;
+    address remittanceController;
+
+    constructor(
+        string memory _name,
+        address _usdc,
+        address _remittanceController
+    ) {
+        name = _name;
+        usdc = _usdc;
+        remittanceController = _remittanceController;
+    }
+
+    function transferToLP(
+        address _targetLP,
+        uint256 _amount,
+        bytes32 _transferalHash
+    ) public {
+        //@todo add AccessControl
+        //@todo add Security Checks
+        //@todo add event with hash
+
+        //Update the Current Fiat Equivalent, that should be in the Fiat-Pool
+        fiatPool += _amount;
+
+        //@todo add transfer
+        LiquidityPool(_targetLP).emitReceive(_amount, _transferalHash);//@audit Probably in Controller
+    }
+
+    function emitReceive(uint256 _amount, bytes32 _transferalHash) public {
+        //@todo add AccessControl
+        //@todo add Security Checks
+
+        //Update the Current Fiat Equivalent, that should be in the Fiat-Pool
+        fiatPool -= _amount;
+
+        //@todo emit Event
+    }
+
+    //@todo Add Equilization
+}
