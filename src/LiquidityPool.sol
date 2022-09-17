27895155c53f18eb215aa75e2d54cd368ded6320
FHieser <felix_hieser@web.de>
2022-09-17 10:08:17 +0200

    Add Events

--- src/LiquidityPool.sol
+++ src/LiquidityPool.sol
@@ -1,16 +1,19 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.13;
 
+import {IERC20} from "lib/openzeppelin-contracts/contracts/token/ERC20/IERC20.sol";
+
 contract LiquidityPool {
     string public name;
     uint256 public fiatPool;
     address public usdc;
     address public remittanceController;
 
-    constructor(
-        string memory _name,
-        address _usdc
-    ) {
+    event transfer(address _targetLP, uint256 _amount, bytes32 _transferalHash);
+
+    event receiveCoin(uint256 _amount, bytes32 _transferalHash);
+
+    constructor(string memory _name, address _usdc) {
         name = _name;
         usdc = _usdc;
     }
@@ -22,12 +25,13 @@ contract LiquidityPool {
     ) public {
         //@todo add AccessControl
         //@todo add Security Checks
-        //@todo add event with hash
 
         //Update the Current Fiat Equivalent, that should be in the Fiat-Pool
         fiatPool += _amount;
 
-        //@todo add transfer
+        IERC20(usdc).transfer(_targetLP, _amount);
+
+        emit transfer(_targetLP, _amount, _transferalHash);
     }
 
     function receivedTokens(uint256 _amount, bytes32 _transferalHash) public {
@@ -37,10 +41,10 @@ contract LiquidityPool {
         //Update the Current Fiat Equivalent, that should be in the Fiat-Pool
         fiatPool -= _amount;
 
-        //@todo add event with hash
+        emit receiveCoin(_amount, _transferalHash);
     }
 
-    //@todo Add Equilization
+    //@todo Add Equalization
 
     function setController(address _controller) public {
         //@todo add AccessControl
